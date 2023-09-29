2128e6b2d22e66ed16c2b836a3c0ed80ab2c54af
jfajaime <140457838+jfajaime@users.noreply.github.com>
2023-09-29 20:15:02 -0300

    se agregan espacios a  un ejercicio

--- src/variables/CalcularIvaFactura.java
+++ src/variables/CalcularIvaFactura.java
@@ -16,9 +16,9 @@ public class CalcularIvaFactura {
         double baseImponible=22.75;
         
         System.out.printf("base imponible %8.2f\n",baseImponible);
-        System.out.printf("IVA            %8.2f\n",(baseImponible*0.21));
-        System.out.printf("-----------------------\n");
-        System.out.printf("Total          %8.2f\n",(baseImponible*1.21));
+        System.out.printf("IVA                     %8.2f\n",(baseImponible*0.21));
+        System.out.printf("--------------------------\n");
+        System.out.printf("Total                  %8.2f\n",(baseImponible*1.21));
     }
 
 }
